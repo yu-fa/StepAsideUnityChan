efd07c1225b8e7686edaf04eaf06bee7c58af430
Masahiro Kotera <m.progress.intly@gmail.com>
2019-09-16 21:26:13 +0900

    Lesson6 課題と発展課題 課題⇒ItemDestroy C#スクリプト作成 発展課題⇒ItemGeneratorを変更

---
+++ Assets/Scripts/ItemDestroy.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ItemDestroy : MonoBehaviour {
+
+    //Unityちゃんのオブジェクト
+    private GameObject unitychan;
+    //UnityちゃんとItemの距離
+    private float itemDistance;
+
+    // Use this for initialization
+    void Start () {
+
+        //Unityちゃんのオブジェクトを取得
+        this.unitychan = GameObject.Find("unitychan");
+
+    }
+	
+	// Update is called once per frame
+	void Update () {
+
+        // Itemとunitychanの距離を計算
+        this.itemDistance = unitychan.transform.position.z - this.transform.position.z;
+
+        if (this.itemDistance > 10 && tag == "CarTag")
+        {
+            Destroy(this.gameObject);
+        }
+
+        else if (this.itemDistance > 10 && tag == "CoinTag")
+        {
+            Destroy(this.gameObject);
+        }
+
+        else if (this.itemDistance > 10 && tag == "TrafficConeTag")
+        {
+            Destroy(this.gameObject);
+        }
+    }
+}
